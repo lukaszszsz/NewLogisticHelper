e0a529e8d1a9d71f56d3e3753eaf00a137085bdb
Mniekyh <58086743+Mniekyh@users.noreply.github.com>
2022-06-06 17:19:54 +0200

    Refractor of connection to DB

--- Logistic-Helper-main/Logistic-Helper/Controllers/TerytController.cs
+++ Logistic-Helper-main/Logistic-Helper/Controllers/TerytController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LogisticHelper.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.ServiceModel;
 
@@ -6,6 +7,12 @@ namespace LogisticHelper.Controllers
 {
     public class TerytController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public TerytController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
         // GET: TerytController
         public ActionResult Index()
         {
@@ -19,12 +26,12 @@ namespace LogisticHelper.Controllers
 
             var result = client.CzyZalogowanyAsync();
 
-            /*result.Status*/
+         
 
 
            //var cos =  client.PobierzListeMiejscowosciWGminieAsync( "śląskie", "gliwicki","gierałtowice",DateTime.Now).Result;
            var wojewodztwa = client.PobierzListeWojewodztwAsync(DateTime.Now).Result;
-            
+         
                 return View(wojewodztwa);
 
 
@@ -34,29 +41,41 @@ namespace LogisticHelper.Controllers
         // GET: TerytController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            serviceteryt.TerytWs1Client client = new serviceteryt.TerytWs1Client();
+
+
+            client.ClientCredentials.UserName.UserName = "Mariusz.Sobota";
+            client.ClientCredentials.UserName.Password = "so6QT8ahG";
+            client.OpenAsync().Wait();
+            
+           
+                string voivode = id.ToString();
+            var getGmina = client.PobierzGminyiPowDlaWojAsync(voivode,DateTime.Now).Result;
+
+
+            return View(getGmina);
         }
 
         // GET: TerytController/Create
-        public ActionResult Create()
+        public ActionResult Search()
         {
+          
             return View();
         }
 
         // POST: TerytController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        //Trzeba zrobić listę ze wszystkimi miastami :/
+        /*public ActionResult Search(string city)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+            serviceteryt.TerytWs1Client client = new serviceteryt.TerytWs1Client();
+
+            client.ClientCredentials.UserName.UserName = "Mariusz.Sobota";
+            client.ClientCredentials.UserName.Password = "so6QT8ahG";
+            client.OpenAsync().Wait();
+           
+        }*/
 
         // GET: TerytController/Edit/5
         public ActionResult Edit(int id)
--- Logistic-Helper-main/Logistic-Helper/Program.cs
+++ Logistic-Helper-main/Logistic-Helper/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
+    builder.Configuration.GetConnectionString("DefaultConnection3")
     )
 );
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
